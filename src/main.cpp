11bf62b2fa8909a6a6edbfd70686887fa3aee2ef
Kevin Kesler Powell <kevinkp@et.byu.edu>
2025-07-30 17:05:53 -0600

    TestCommit

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,5 @@
 /***************************************************
-  This is our library for the Adafruit HX8357D Breakout
-  ----> http://www.adafruit.com/products/2050
-
-  Check out the links above for our tutorials and wiring diagrams
-  These displays use SPI to communicate, 4 or 5 pins are required to
-  interface (RST is optional)
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.
-  MIT license, all text above must be included in any redistribution
+This is my attempt to get touch and temp going
  ****************************************************/
 
 #include <SPI.h>
